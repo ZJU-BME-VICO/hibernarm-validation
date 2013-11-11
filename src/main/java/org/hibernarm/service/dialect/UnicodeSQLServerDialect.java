921f149e9b92956cd35a2758f1f6d20d531a9b07
daowangli@gmail.com <daowangli@gmail.com>
2013-11-11 23:35:29 +0800

    #10 Update unicode dialect for sql server

--- src/main/java/org/hibernarm/service/dialect/UnicodeSQLServerDialect.java
+++ src/main/java/org/hibernarm/service/dialect/UnicodeSQLServerDialect.java
@@ -3,21 +3,29 @@ package org.hibernarm.service.dialect;
 import java.sql.Types;
 
 import org.hibernate.dialect.SQLServerDialect;
+import org.hibernate.type.StandardBasicTypes;
 
 public class UnicodeSQLServerDialect extends SQLServerDialect {
 
 	public UnicodeSQLServerDialect() {
-		super();
-		
-//        registerColumnType(Types.BIGINT, "bigint");
-//        registerColumnType(Types.BIT, "bit");
-//        registerColumnType(Types.CHAR, "nchar(1)");
+        super();
+        
+        registerColumnType(Types.CHAR, "nchar(1)");
+        registerColumnType(Types.LONGVARCHAR, "nvarchar(max)" );
         registerColumnType(Types.VARCHAR, 4000, "nvarchar($l)");
-//        registerColumnType(Types.VARCHAR, "nvarchar(max)");
-//        registerColumnType(Types.VARBINARY, 4000, "varbinary($1)");
-//        registerColumnType(Types.VARBINARY, "varbinary(max)");
-//        registerColumnType(Types.BLOB, "varbinary(max)");
-        registerColumnType(Types.CLOB, "nvarchar(max)");
+        registerColumnType(Types.VARCHAR, "nvarchar(max)");
+        registerColumnType(Types.CLOB, "nvarchar(max)" );
+
+        registerColumnType(Types.NCHAR, "nchar(1)");
+        registerColumnType(Types.LONGNVARCHAR, "nvarchar(max)");
+        registerColumnType(Types.NVARCHAR, 4000, "nvarchar($l)");
+        registerColumnType(Types.NVARCHAR, "nvarchar(max)");
+        registerColumnType(Types.NCLOB, "nvarchar(max)");
+
+        registerHibernateType(Types.NCHAR, StandardBasicTypes.CHARACTER.getName());
+        registerHibernateType(Types.LONGNVARCHAR, StandardBasicTypes.TEXT.getName());
+        registerHibernateType(Types.NVARCHAR, StandardBasicTypes.STRING.getName());
+        registerHibernateType(Types.NCLOB, StandardBasicTypes.CLOB.getName() );
 	}
 
 }
