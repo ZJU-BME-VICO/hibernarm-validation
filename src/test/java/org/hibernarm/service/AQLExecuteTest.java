e3eda07354ba0b73e46f443d8c93f59b46b75ab9
daowangli@gmail.com <daowangli@gmail.com>
2013-12-19 16:05:59 +0800

    #21 Configure hibernate connection pool

--- src/test/java/org/hibernarm/service/AQLExecuteTest.java
+++ src/test/java/org/hibernarm/service/AQLExecuteTest.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
